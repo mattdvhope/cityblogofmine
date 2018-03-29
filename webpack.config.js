bb83309944f4c8241fa5f9838cca1a8e1bc44923
mattdvhope <mattagape@me.com>
2018-03-29 10:05:10 -0400

    Worked on making webpack/react production ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   // webpack folder’s entry js — excluded from jekll’s build process.
   entry: "./webpack/entry.js",
   output: {
-    path: path.join(__dirname, 'src/assets/javascripts'), // 'build' here is a folder that webpack creates which contains 'bundle.js'
-    filename: 'bundle.js' // 'bundle.js' is inside the 'build' folder that had been created
+    // path: path.join(__dirname, 'src/assets/javascripts'), // 'build' here is a folder that webpack creates which contains 'bundle.js'
+    // filename: 'bundle.js' // 'bundle.js' is inside the 'build' folder that had been created
+
+    // move bundle.js to a folder instead of the root 
+    path: path.resolve('./src/assets/build'),
+    filename: 'bundle.js'
+
   },
   module: {
     rules: [
@@ -20,6 +28,15 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      // injects bundle.js to our new index.html
+      inject: true,
+      // copys the content of the existing index.html to the new /build index.html
+      template:  path.resolve('./src/index.html'),
+    })
+  ],
   mode: 'development'
 };
 
